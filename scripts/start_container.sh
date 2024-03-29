e4be8876344a113bfd62baba06fed3c9cc091979
surakshamundkur <154885639+surakshamundkur@users.noreply.github.com>
2024-03-29 15:41:38 +0530

    ecr login

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-
-# Pull the Docker image from Docker Hub
-docker pull 921932246560.dkr.ecr.us-east-1.amazonaws.com/demo_sample
-
+# Pull the Docker image from Amazon ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 921932246560.dkr.ecr.us-east-1.amazonaws.com
+docker pull 921932246560.dkr.ecr.us-east-1.amazonaws.com/demo_sample:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000 921932246560.dkr.ecr.us-east-1.amazonaws.com/demo_sample
+docker run -d -p 5000:5000 921932246560.dkr.ecr.us-east-1.amazonaws.com/demo_sample:latest
