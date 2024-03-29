c32c30ecdf7ed31bcd1ee25a7ed6a9964f9045a9
surakshamundkur <154885639+surakshamundkur@users.noreply.github.com>
2024-03-29 14:30:37 +0530

    added ecr registory

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull 921932246560.dkr.ecr.us-east-1.amazonaws.com/demo_sample
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 921932246560.dkr.ecr.us-east-1.amazonaws.com/demo_sample
